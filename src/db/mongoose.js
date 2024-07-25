ade5a6d07bdc417964a2e31596998eb9bd617c5b
SayyamJain <sayyam.jain1505@gmail.com>
2024-07-25 17:56:48 +0530

    Init commit

---
+++ src/db/mongoose.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("Error conecting to MongoDB:", error);
+  });
